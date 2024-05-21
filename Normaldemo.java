8e13b5978efad8027e2d0d4e623477672a89b3db
monaa123456789 <168552705+monaa123456789@users.noreply.github.com>
2024-05-21 12:41:02 +0530

    normaldemo file uploaded

---
+++ Normaldemo.java
@@ -0,0 +1,22 @@
+package MyPackage;
+
+public class Normaldemo {
+	int a=12;
+	static String b ="eysfyug";
+	String  display() {
+		return "hello";
+	}
+		public static String display1() {
+			return "chitti";
+		}
+		public static void main(String[] args) {
+			Normaldemo n1=new Normaldemo();
+			System.out.println(n1.a);
+			Normaldemo.display1();
+			System.out.println(b);
+			
+			
+			
+		}
+	}
+	
